08b243604103975a706f2f713a709443d8488a64
Arpit Yadav <arpit526yadav@gmail.com>
2026-03-20 22:23:57 +0530

    prediction part connected to frontend

---
+++ frontend/src/types/churn.ts
@@ -0,0 +1,28 @@
+export type CustomerPayload = {
+  gender: string;
+  SeniorCitizen: number;
+  Partner: string;
+  Dependents: string;
+  tenure: number;
+  PhoneService: string;
+  MultipleLines: string;
+  InternetService: string;
+  OnlineSecurity: string;
+  OnlineBackup: string;
+  DeviceProtection: string;
+  TechSupport: string;
+  StreamingTV: string;
+  StreamingMovies: string;
+  Contract: string;
+  PaperlessBilling: string;
+  PaymentMethod: string;
+  MonthlyCharges: number;
+  TotalCharges: number;
+};
+
+export type PredictionResponse = {
+  churn_probability: number;
+  churn_prediction: number;
+  risk_level: string;
+  retention_action: string;
+};
