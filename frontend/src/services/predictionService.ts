96fd029e3af42071e96e6d11277b0cba0a51ffab
Arpit Yadav <arpit526yadav@gmail.com>
2026-03-23 10:28:22 +0530

    frontend replaced with streamlite

--- frontend/src/services/predictionService.ts
+++
@@ -1,9 +0,0 @@
-import { api } from "../lib/api";
-import type { CustomerPayload, PredictionResponse } from "../types/churn";
-
-export async function predictCustomer(
-  payload: CustomerPayload
-): Promise<PredictionResponse> {
-  const response = await api.post<PredictionResponse>("/predict", payload);
-  return response.data;
-}
